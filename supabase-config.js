f7b3dcbb8b460cedf61baf1a45fbbb26f93cd0af
Soufiane Kerrouche <soufkerr05@gmail.com>
2026-05-07 09:23:18 +0100

    log in page

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,11 @@
+// إعدادات الاتصال بقاعدة البيانات
 const { createClient } = supabase;
 const supabaseUrl = 'https://klkvblbtttklzaypnokl.supabase.co';
 const supabaseKey = 'sb_publishable_vnrYeqJxy1OQIwaEvdnc_A_H2R9IC3v';
 const _supabase = createClient(supabaseUrl, supabaseKey);
+
+function logoutUser() {
+    localStorage.removeItem('karate_auth');
+    localStorage.removeItem('karate_auth_time');
+    window.location.replace('login.html');
+}
